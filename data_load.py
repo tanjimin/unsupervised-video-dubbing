9eb9518a2a79b1c65cb7bbbe341dd853694652de
Jimin Tan <tanjimin@hotmail.com>
2020-02-23 18:20:04 -0500

    randomization changed

--- data_load.py
+++ data_load.py
@@ -23,20 +23,11 @@ class LRW_Dataset_AV():
         with open(self.labels_file) as myfile:
             self.data_dir = myfile.read().splitlines()
             
-        self.v_list = {}
-        self.a_list = {}
-        
         self.video_file = os.path.join(self.root_path, 'video_' + self.folds+ '.npy')
         self.audio_file = os.path.join(self.root_path, 'audio_' + self.folds +'.npy')
   
-        if self.folds == 'test':
-            self.video = npy_loader_aug_test(self.video_file, v_flag = 1)
-            self.audio = npy_loader_aug_test(self.audio_file, v_flag = 0)
-        else:
-            self.video = npy_loader_aug(self.video_file, v_flag = 1)
-            self.audio = npy_loader_aug(self.audio_file, v_flag = 0)
-            
-        self.labels = 0
+        self.video = npy_loader(self.video_file)
+        self.audio = npy_loader(self.audio_file)
         
         print('Loading {} part'.format(self.folds))
 
@@ -44,44 +35,20 @@ class LRW_Dataset_AV():
         return self.video.shape[0]
 
     def __getitem__(self, idx):
-            
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        
-        vid = self.video[idx, :, :, :]
+        vid = self.augmentation(self.video[idx, :, :, :])
         aud = self.audio[idx, :, :]
         labels = 0
+        return (vid, aud), labels    
         
-        sample = (vid, aud), labels
-        
-        return sample
-
-def npy_loader_aug(file, v_flag):
-    
-    data = np.load(file)
-    if v_flag == 1:
-        keypoints = torch.tensor(data).float()
-
+    def augmentation(self, keypoints):
         keypoints_move = keypoints * 0.7
         ones = torch.ones(keypoints.shape, dtype = torch.float)
         randint = torch.randint(1,73,(1,),dtype = torch.float)
-        d = keypoints_move + ones * randint
-    
-    else:
-        d = torch.tensor(data).float()
-    return d
+        if self.folds == 'train':
+            d = keypoints_move + ones * randint
+        else:
+            d = keypoints_move + ones * 38
+        return d
 
-def npy_loader_aug_test(file, v_flag):
-    
-    data = np.load(file)
-    if v_flag == 1:
-        keypoints = torch.tensor(data).float()
-
-        keypoints_move = keypoints * 0.7
-        ones = torch.ones(keypoints.shape, dtype = torch.float)
-        randint = torch.randint(1,73,(1,),dtype = torch.float)
-        d = keypoints_move + ones * 38
-    
-    else:
-        d = torch.tensor(data).float()
-    return d
+def npy_loader(file):
+    return torch.tensor(np.load(file)).float()
---
+++ validation/data_test.py
@@ -0,0 +1,5 @@
+import sys
+sys.append('..')
+
+dataset = LRW_Dataset_AV()
+import pdb; pbd.set_trace()
